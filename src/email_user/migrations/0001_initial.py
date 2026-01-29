973ecddfa09da96c81612d0cafd5f8e9608d1e89
eumas42 <eugen.massini@solve42.de>
2026-01-29 18:18:26 +0100

    remved dependency

--- src/email_user/migrations/0001_initial.py
+++ src/email_user/migrations/0001_initial.py
@@ -11,9 +11,7 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-        ("auth", "0007_alter_validators_add_error_messages"),
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
